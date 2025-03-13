7f6913b5c34f78420909ae13fee8f736e0bb5f69
Kaustav Surai <kaustav.surai@opencastsoftware.com>
2025-03-13 23:06:08 +0000

    updated

---
+++ notifySlack.js
@@ -0,0 +1,45 @@
+import { argv, version } from 'yargs';
+import { SNS } from 'aws-sdk';
+const summary = require('allure-report/widgets/summary.json');
+
+async function notifySlack() {
+    const { name, success, report, buildId, startTime } = argv;
+
+    const sns = new SNS({
+        region: 'us-east-2',
+    });
+
+    const [projectName, id] = buildId.split(':');
+
+    const parsedStartTime = new Date(startTime).toLocaleTimeString('en-GB', {
+        timeZone: 'Europe/London',
+    });
+
+    const { failed, total } = summary.statistic;
+
+    const description = [
+        `<${report}|Allure Report>`,
+        `<https://us-east-2.console.aws.amazon.com/codesuite/codebuild/390403859997/projects/${projectName}/build/${projectName}%3A${id}?region=us-east-2|CodeBuild>`,
+        `Start time: ${parsedStartTime}`
+    ].join(' | ');
+
+    console.info('Allure reports', report);
+
+    sns.publish({
+        Message: JSON.stringify({
+            version: '1.0',
+            source: 'custom',
+            content: {
+                title: `QA Run: \`${name}\` ${success.toString() === '1'
+                        ? 'was successful ✅'
+                        : `failed (${failed}/${total}) ❌`
+                    }`,
+                description,
+            },
+        }),
+        TopicArn:
+            'arn:aws:sns:us-east-2:390403859997:playwright-slack-notification'
+    }).promise();
+}
+
+notifySlack();
