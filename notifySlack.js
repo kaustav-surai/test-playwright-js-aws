20f022cf264e0bbbb5229759543da2f7bdaa2a9a
Kaustav Surai <kaustav.surai@opencastsoftware.com>
2025-03-14 13:53:53 +0000

    cleanup

--- notifySlack.js
+++ notifySlack.js
@@ -2,14 +2,6 @@ import yargs from 'yargs/yargs';
 import AWS from 'aws-sdk';
 
 async function notifySlack() {
-    const argv = await yargs(process.argv.slice(2))
-        .option('name', { type: 'string', description: 'Name of the QA run', demandOption: true })
-        .option('success', { type: 'string', description: 'Result of the QA run (e.g., "true", "false", "1", "0")', demandOption: true })
-        .option('report', { type: 'string', description: 'URL to the Allure report', demandOption: true })
-        .option('buildId', { type: 'string', description: 'CodeBuild build ID (projectName:id)', demandOption: true })
-        .option('startTime', { type: 'string', description: 'Start time of the QA run (ISO format or format parsable by Date)', demandOption: true })
-        .option('region', { type: 'string', default: 'us-east-2', description: 'AWS region for SNS' })
-        .argv;
 
     const { name, success, report, buildId, startTime, region } = argv;
 
@@ -22,21 +14,14 @@ async function notifySlack() {
 
     const [projectName, id] = buildId.split(':');
 
-    const parsedStartTime = new Date(startTime); // Parse startTime directly
-    console.log('Parsed startTime:', parsedStartTime); // Log the result of parsing
-
-    const isValidDate = parsedStartTime instanceof Date && !isNaN(parsedStartTime.valueOf());
-
-    const formattedStartTime = isValidDate
-        ? parsedStartTime.toLocaleTimeString('en-GB', {
-            timeZone: 'Europe/London',
-        })
-        : 'Invalid Date';
+    const parsedStartTime = new Date(startTime).toLocaleDateString('en-GB',{
+        timeZone:'Europe/London',
+    });
 
     const description = [
         `<${report}|Report>`,
         `<https://us-east-2.console.aws.amazon.com/codesuite/codebuild/390403859997/projects/${projectName}/build/${projectName}%3A${id}?region=${region}|CodeBuild>`,
-        `Start time: ${formattedStartTime}`
+        `Start time: ${parsedStartTime}`
     ].join(' | ');
 
     try {
