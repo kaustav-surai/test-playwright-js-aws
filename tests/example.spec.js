984b6d2f96590b00d95f0ed1213a6e8ab532f080
Kaustav Surai <kaustav.surai@opencastsoftware.com>
2025-03-14 18:55:26 +0000

    add

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -8,6 +8,13 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
+test('has wrong title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Selenium/);
+});
+
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
